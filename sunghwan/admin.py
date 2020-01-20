7e8831d90dea0b8508f43669ae84d4bddc6db52e
seungwoo <leemoney93@naver.com>
2020-01-20 09:45:38 +0900

    admin 변경2

--- sunghwan/admin.py
+++ sunghwan/admin.py
@@ -4,8 +4,15 @@ from .models import *
 
 @admin.register(ProbesGroup)
 class ProbesGroupAdmin(admin.ModelAdmin):
-    list_display = ['*']
-    # search_fields = ['username', 'first_name']
+    list_display = ['DateTime', 'Internal_average_temperature', 'Internal_humidity', 'External_temperature',
+                    'External_humidity', 'Pressure', 'Fan_temperature_1', 'Fan_temperature_2', 'Fan_temperature_3',
+                    'Fan_temperature_4', 'Fan_temperature_5', 'Fan_temperature_6', 'Window_temperature_1',
+                    'Internal_Temperature_Minimum_Daily', 'Internal_Temperature_Average_Daily',
+                    'Internal_Temperature_Maximum_Daily', 'Internal_Humidity_Minimum_Daily',
+                    'Internal_Humidity_Average_Daily', 'Internal_Humidity_Maximum_Daily',
+                    'External_Temperature_Minimum_Daily', 'External_Temperature_Average_Daily',
+                    'External_Temperature_Maximum_Daily', 'External_Humidity_Minimum_Daily',
+                    'External_Humidity_Average_Daily', 'External_Humidity_Maximum_Daily']
     model = ProbesGroup
 
 
